fbd7752a0fc521d33c8af7f4bd91c77ce0e93a83
vincent dubuc <vincent@lizard.global>
2024-07-04 11:43:56 +0800

    - Ajout de la fonctionnalité pour upload des pdf

--- app/concours/[name]/[subject]/_components/video/_components/sectionForm/ContextMenuSectionAction.tsx
+++ app/concours/[name]/[subject]/_components/video/_components/sectionForm/ContextMenuSectionAction.tsx
@@ -47,20 +47,17 @@ export function ContextMenuSectionAction({title, idSection}: ContextMenuSectionA
             });
         });
     }
+
+
     return (
         <ContextMenuContent>
             <ContextMenuItem onClick={onDelete}>
                 Supprimer la section {title}
-          </ContextMenuItem>
+            </ContextMenuItem>
+
             <DialogTrigger asChild>
                 <ContextMenuItem>Modifier la section {title}</ContextMenuItem>
             </DialogTrigger>
-            <ContextMenuItem onClick={()=>{}}>
-                Ajouter la fiche de la section {title}
-            </ContextMenuItem>
-            <ContextMenuItem onClick={()=>{}}>
-                Supprimer la fiches de la section {title}
-            </ContextMenuItem>
-      </ContextMenuContent>
-  );
+        </ContextMenuContent>
+    );
 }
